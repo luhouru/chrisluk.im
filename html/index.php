37ca74477c1ab14b51feef7c3bf1f0a1287162d4
Chris Luk <chriseluk@gmail.com>
2014-07-17 19:48:29 -0400

    title

--- html/index.php
+++ html/index.php
@@ -37,7 +37,7 @@ if (isset($_GET['action'])) {
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Chris Luk | Programmer, Designer, </title>
+		<title>Chris Luk | Developer </title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
