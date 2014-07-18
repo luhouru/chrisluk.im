aa05080717c6e9e713a02adcd467954f53361fd0
Chris Luk <chriseluk@gmail.com>
2014-07-18 17:39:11 -0400

    7/18 changes

--- html/index.php
+++ html/index.php
@@ -86,42 +86,35 @@ if (isset($_GET['action'])) {
 		
 		<!-- Feature 1 -->
 			<article id="first" class="container box style1 right">
-				<a href="http://www.google.com" class="image full"><img src="images/pic01.jpg" alt="" /></a>
+				<a href="http://www.chrisluk.im/ratriot" class="image full"><img src="images/pic01.jpg" alt="" /></a>
 				<div class="inner">
 					<header>
-						<h2 style="color:white;">Hello There!<br />
-						PROJECT 1</h2>
+						<h2 style="color:white;">Rat<br />
+						Riot</h2>
 					</header>
-					<p>Why don't you just love this blank white space?</p>
+					<p style="color:white;">Simple JavaScript game using HTML5.</p>
 				</div>
 			</article>
 		
 		<!-- Feature 2 -->
 			<article class="container box style1 left">
-				<a href="http://www.google.com" class="image full"><img src="images/pic02.jpg" alt="" /></a>
+				<a href="http://www.chrisluk.im/nashor" class="image full"><img src="images/pic02.png" alt="" /></a>
 				<div class="inner">
 					<header>
-						<h2>Still Here?<br />
-						You must love me.</h2>
+						<h2 style="color:white;">NashorDB<br />League of Legends dashboard.</h2>
 					</header>
-					<p>Why is this picture black?</p>
+					<p style="color:white;">Statistics and database management system.</p>
 				</div>
 			</article>
 		
 		<!-- Portfolio -->
 			<article class="container box style2">
 				<header>
-					<h2>This Latin is disturbing.</h2>
-					<p>I need to get rid of it.<br />
-					Cleaning up.</p>
+					<h2 style="color:white;">PROJECTS</h2>
+					<p style="color:white;">See all the current projects<br />
+					I've been working on!</p>
 				</header>
 				<div class="inner gallery">
-					<div class="row flush">
-						<div class="3u"><a href="images/fulls/01.jpg" class="image full"><img src="images/thumbs/01.jpg" alt="" title="Ad infinitum" /></a></div>
-						<div class="3u"><a href="images/fulls/02.jpg" class="image full"><img src="images/thumbs/02.jpg" alt="" title="Dressed in Clarity" /></a></div>
-						<div class="3u"><a href="images/fulls/03.jpg" class="image full"><img src="images/thumbs/03.jpg" alt="" title="Raven" /></a></div>
-						<div class="3u"><a href="images/fulls/04.jpg" class="image full"><img src="images/thumbs/04.jpg" alt="" title="I'll have a cup of Disneyland, please" /></a></div>
-					</div>
 					<div class="row flush">
 						<div class="3u"><a href="images/fulls/05.jpg" class="image full"><img src="images/thumbs/05.jpg" alt="" title="Cherish" /></a></div>
 						<div class="3u"><a href="images/fulls/06.jpg" class="image full"><img src="images/thumbs/06.jpg" alt="" title="Different." /></a></div>
@@ -132,36 +125,37 @@ if (isset($_GET['action'])) {
 			</article>
 		
 		<!-- Contact -->
-			<article class="container box style3">
-				<header>
-					<h2 style="color:white;">CONTACT ME</h2>
-					<p style="color:white;">Send me a message!</p>
-				</header>
-				<form action="index.php?action=sendemail" method="POST" role="form">
-					<div class="row half">
-						<div class="6u"><input type="text" class="text" name="name" placeholder="Name" /></div>
-						<div class="6u"><input type="text" class="text" name="email" placeholder="Email" /></div>
-					</div>
-					<div class="row half">
-						<div class="12u">
-							<textarea type="text" name="message" placeholder="Message"></textarea>
-						</div>
-					</div>
-					<div class="row">
-						<div class="12u">
-							<ul class="actions">
-								<li><input type="submit" class="button style2 scrolly" value="    Send Message    " /></li>
-							</ul>
-						</div>
-					</div>
-				</form>
-			</article>
+            <section id="contact">
+                <article class="container box style3">
+                    <header>
+                        <h2 style="color:white;">CONTACT ME</h2>
+                        <p style="color:white;">Send me a message!</p>
+                    </header>
+                    <form action="index.php?action=sendemail" method="POST" role="form">
+                        <div class="row half">
+                            <div class="6u"><input type="text" class="text" name="name" placeholder="Name" /></div>
+                            <div class="6u"><input type="text" class="text" name="email" placeholder="Email" /></div>
+                        </div>
+                        <div class="row half">
+                            <div class="12u">
+                                <textarea type="text" name="message" placeholder="Message"></textarea>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="12u">
+                                <ul class="actions">
+                                    <li><input type="submit" class="button style2 scrolly" value="    Send Message    " /></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </form>
+                </article>
+            </section>
 		<section id="footer">
 			<ul class="icons">
 				<li><a href="#" class="fa fa-twitter solo"><span>Twitter</span></a></li>
 				<li><a href="http://www.facebook.com/cel2971" class="fa fa-facebook solo"><span>Facebook</span></a></li>
-				<li><a href="luk@chrisluk.im" class="fa fa-gmail solo"><span>Email</span></a></li>
-				<li><a href="#" class="fa fa-pinterest solo"><span>Pinterest</span></a></li>
+				<li><a href="#contact" class="fa fa-envelope solo"><span>Email</span></a></li>
 				<li><a href="http://www.github.com/cluk2971" class="fa fa-github solo"><span>GitHub</span></a></li>
 				<li><a href="http://www.linkedin.com/pub/chris-luk/67/ba6/62a/" class="fa fa-linkedin solo"><span>LinkedIn</span></a></li>
 			</ul>
