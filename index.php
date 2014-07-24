89460243093c783085b9bac9a1dd4816cd339bce
cluk2971 <chris.luk@continuum.net>
2014-07-24 16:16:39 -0400

    updated 724

--- index.php
+++ index.php
@@ -174,9 +174,9 @@
 								<section>
 									<a href="#" class="image feature"><img src="images/pic01.jpg" alt="" /></a>
 									<header>
-										<h3>A Really Fast Train</h3>
+										<h3>NashorDB</h3>
 									</header>
-									<p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
+									<p>Database Management dashboard for League of Legends.</p>
 								</section>
 
 							</div>
@@ -185,9 +185,9 @@
 								<section>
 									<a href="#" class="image feature"><img src="images/pic02.jpg" alt="" /></a>
 									<header>
-										<h3>An Airport Terminal</h3>
+										<h3>JavaScript Game</h3>
 									</header>
-									<p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
+									<p>TBC</p>
 								</section>
 
 							</div>
@@ -198,9 +198,9 @@
 								<section>
 									<a href="#" class="image feature"><img src="images/pic03.jpg" alt="" /></a>
 									<header>
-										<h3>Hyperspace Travel</h3>
+										<h3>Project 3</h3>
 									</header>
-									<p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
+									<p>Third times the charm.</p>
 								</section>
 
 							</div>
@@ -209,7 +209,7 @@
 								<section>
 									<a href="#" class="image feature"><img src="images/pic04.jpg" alt="" /></a>
 									<header>
-										<h3>And Another Train</h3>
+										<h3>Project 4</h3>
 									</header>
 									<p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
 								</section>
