beb235442b84943224bd5526c3ef3e152392f22c
cluk2971 <chris.luk@continuum.net>
2014-08-22 10:14:54 -0400

    added smartchatbox

--- index.php
+++ index.php
@@ -216,7 +216,9 @@
                     <a class="twitter-timeline" href="https://twitter.com/rukthenuke" data-widget-id="500646003737767937">Tweets by @rukthenuke</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 				</footer>
-                <br><br><br><br>
+                <br><br>
+                <script type="text/javascript" src="http://www3.smartchatbox.com/shoutbox/start.php?key=970744588"></script>    
+                <br><br>
                 <script type="text/javascript" src="http://www.brainyquote.com/link/quotebr.js"></script><br>
 			</section>
 		<!-- Footer -->
