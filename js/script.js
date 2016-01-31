a295df213ac0f39a9279823e6fde4d643e01243b
Chris Luk <luho@chrisluk.im>
2016-01-31 01:02:16 -0500

    nickname change

--- js/script.js
+++ js/script.js
@@ -168,7 +168,7 @@ $(document).ready(function(){
 	setTimeout( function(){
 		if($("#site-type").length) {
         $(".typewrite span").typed({
-            strings: ["RUKTHENUKE ","LAIDBACK ", "CHRIS LUK "],
+            strings: ["LAIDBACK ","RUKTHENUKE ", "CHRIS LUK "],
             typeSpeed: 100,
             backDelay: 500,
             loop: false,
